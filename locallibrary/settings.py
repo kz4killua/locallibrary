48a2f4fbb00c4202cb358ce298a3f7f748a63a68
Ifeanyi Obinelo <62487964+KZ4Killua@users.noreply.github.com>
2022-07-23 00:00:31 +0100

    Update ALLOWED_HOSTS in settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-kr=&t-c27vaerb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kz-locallibrary.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
